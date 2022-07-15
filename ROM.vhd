548dd39bf68a95c11652abe727ec6611f98279c0
Vinícius Cardoso <vinicius.mct17@gmail.com>
2022-07-15 19:36:27 -0300

    corrigindo erros

--- ROM.vhd
+++ ROM.vhd
@@ -2,24 +2,23 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- MEMÓRIA EEPROM
-entity eeprom is
+entity rom is
 	generic (
 		ADDR_LENGHT : natural := 2;
 		R_LENGHT : natural := 16;
-		NUM_OF_REGS : natural := 32
+		REGS : natural := 16
 	);
 	port (
 		clk : in std_logic;
 		wr : in std_logic;
 		addr : in std_logic_vector (ADDR_LENGHT - 1 downto 0);
-		datain : in std_logic_vector (R_LENGHT - 1 downto 0);
-		dataout : out std_logic_vector (R_LENGHT - 1 downto 0)
+		data_in : in std_logic_vector (R_LENGHT - 1 downto 0);
+		data_out : out std_logic_vector (R_LENGHT - 1 downto 0)
 	);
 end entity;
 
 architecture rom_register of eeprom is
-	type rom_reg is array (0 to NUM_OF_REGS - 1) of
+	type rom_reg is array (0 to REGS - 1) of
 	std_logic_vector(R_LENGHT - 1 downto 0);
 	signal rom_s : rom_reg;
 
@@ -30,9 +29,9 @@ begin
 		if (rising_edge(clk)) then
 			loc := to_integer(unsigned(addr));
 			if (wr = '1') then
-				rom_s(loc) <= datain;
+				rom_s(loc) <= data_in;
 			end if;
-			dataout <= rom_s(loc);
+			data_out <= rom_s(loc);
 		end if;
 	end process;
 end architecture rom_register;
